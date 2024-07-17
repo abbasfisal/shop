43cfb91d17dd3b60e8b6fa9bac8209c6c24a2aca
abbas <abbasmomeny1994@gmail.com>
2024-07-17 23:26:50 +0330

    ref: add parentId to category to implement subCategories

--- internal/entities/category.go
+++ internal/entities/category.go
@@ -4,10 +4,11 @@ import "gorm.io/gorm"
 
 type Category struct {
 	gorm.Model
-	Title    string `gorm:"type:varchar(150);not null"`
-	Slug     string `gorm:"type:varchar(150);not null;unique"`
-	ParentID uint
-	Image    string
-	Status   bool      `gorm:"default true;no null"`
-	Products []Product `gorm:"foreignKey:CategoryID"`
+	Title         string `gorm:"type:varchar(150);not null"`
+	Slug          string `gorm:"type:varchar(150);not null;unique"`
+	ParentID      *uint
+	Image         string
+	Status        bool       `gorm:"default true;no null"`
+	SubCategories []Category `gorm:"foreignKey:ParentID"`
+	Products      []Product  `gorm:"foreignKey:CategoryID"`
 }
