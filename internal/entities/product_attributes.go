9ec81e2313f0eca4ac2f9451e87011579b904852
abbas <abbasmomeny1994@gmail.com>
2024-07-26 12:45:25 +0330

    add: product_attributes entity

---
+++ internal/entities/product_attributes.go
@@ -0,0 +1,17 @@
+package entities
+
+import "gorm.io/gorm"
+
+type ProductAttribute struct {
+	gorm.Model
+	ProductID uint
+
+	AttributeID    uint
+	AttributeTitle string
+
+	AttributeValueID    uint
+	AttributeValueTitle string
+
+	Attribute      Attribute      `gorm:"foreignKey:AttributeID"`
+	AttributeValue AttributeValue `gorm:"foreignKey:AttributeValueID"`
+}
