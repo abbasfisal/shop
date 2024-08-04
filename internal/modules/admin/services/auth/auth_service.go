925409b8ffb1bb53888cd681eed92d62d8d5b4b4
abbas <abbasmomeny1994@gmail.com>
2024-08-04 15:25:45 +0330

    ref: auth_service

--- internal/modules/admin/services/auth/auth_service.go
+++ internal/modules/admin/services/auth/auth_service.go
@@ -2,9 +2,7 @@ package auth
 
 import (
 	"context"
-	"errors"
 	"golang.org/x/crypto/bcrypt"
-	"gorm.io/gorm"
 	"log"
 	"shop/internal/modules/admin/repositories/auth"
 	"shop/internal/modules/admin/requests"
@@ -23,21 +21,18 @@ func NewAuthenticateService(authRepo auth.AuthenticateRepositoryInterface) Authe
 }
 
 func (a AuthenticateService) Login(ctx context.Context, req requests.LoginRequest) (AdminUserResponse.User, custom_error.CustomError) {
-	var customErr custom_error.CustomError
-	var userResponse AdminUserResponse.User
-	user, err := a.authRepo.FindBy(ctx, req.Mobile)
 
+	user, err := a.authRepo.FindBy(ctx, req.Mobile)
 	if user.ID == 0 {
 		log.Println("[findUserBy error ] : ", err)
-		if errors.Is(err, gorm.ErrRecordNotFound) {
-			return userResponse, custom_error.New(err.Error(), custom_error.MobileOrPasswordIsWrong, 404)
+		if err != nil {
+			return AdminUserResponse.User{}, custom_error.HandleError(err, custom_error.RecordNotFound)
 		}
-		return userResponse, custom_error.New(err.Error(), custom_error.InternalServerError, 500)
 	}
 
 	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(req.Password)); err != nil {
-		return userResponse, custom_error.New(err.Error(), custom_error.MobileOrPasswordIsWrong, 404)
+		return AdminUserResponse.User{}, custom_error.New(err.Error(), custom_error.MobileOrPasswordIsWrong, 404)
 	}
 
-	return AdminUserResponse.ToUserResponse(user), customErr
+	return AdminUserResponse.ToUserResponse(user), custom_error.CustomError{}
 }
