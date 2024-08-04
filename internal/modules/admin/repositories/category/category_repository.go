b81f459c0e1ae2e8816f5ad33397dccc03f20926
abbas <abbasmomeny1994@gmail.com>
2024-08-04 15:38:20 +0330

    ref: category_repository use WithContext() 😝

--- internal/modules/admin/repositories/category/category_repository.go
+++ internal/modules/admin/repositories/category/category_repository.go
@@ -17,31 +17,30 @@ func NewCategoryRepository(db *gorm.DB) CategoryRepository {
 
 func (cr CategoryRepository) GetAll(ctx context.Context) ([]entities.Category, error) {
 	var categories []entities.Category
-	err := cr.db.Find(&categories).Error
+	err := cr.db.WithContext(ctx).Find(&categories).Error
 	return categories, err
 }
 
 func (cr CategoryRepository) GetAllParent(ctx context.Context) ([]entities.Category, error) {
 	var categories []entities.Category
-	err := cr.db.Where("parent_id IS NULL").Find(&categories).Error
+	err := cr.db.WithContext(ctx).Where("parent_id IS NULL").Find(&categories).Error
 	return categories, err
 }
 
 func (cr CategoryRepository) SelectBy(ctx context.Context, categoryID int) (entities.Category, error) {
 	var category entities.Category
-	err := cr.db.First(&category, "id=?", categoryID).Error
-
+	err := cr.db.WithContext(ctx).First(&category, "id = ?", categoryID).Error
 	return category, err
 }
 
 func (cr CategoryRepository) FindBy(ctx context.Context, columnName string, value any) (entities.Category, error) {
-	var cat entities.Category
+	var category entities.Category
 	condition := fmt.Sprintf("%s = ?", columnName)
-	err := cr.db.First(&cat, condition, value).Error
-	return cat, err
+	err := cr.db.WithContext(ctx).First(&category, condition, value).Error
+	return category, err
 }
 
-func (cr CategoryRepository) Store(ctx context.Context, cat entities.Category) (entities.Category, error) {
-	err := cr.db.Create(&cat).Error
-	return cat, err
+func (cr CategoryRepository) Store(ctx context.Context, category entities.Category) (entities.Category, error) {
+	err := cr.db.WithContext(ctx).Create(&category).Error
+	return category, err
 }
