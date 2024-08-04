4590f5cb306deb434279c6f3606ffebc4099c544
abbas <abbasmomeny1994@gmail.com>
2024-08-04 15:20:36 +0330

    ref: category_serice ,use HanldeError()

--- internal/modules/admin/services/category/category_service.go
+++ internal/modules/admin/services/category/category_service.go
@@ -2,8 +2,6 @@ package category
 
 import (
 	"context"
-	"errors"
-	"gorm.io/gorm"
 	"shop/internal/entities"
 	"shop/internal/modules/admin/repositories/category"
 	"shop/internal/modules/admin/requests"
@@ -22,30 +20,19 @@ func NewCategoryService(categoryRepo category.CategoryRepositoryInterface) Categ
 }
 func (cs CategoryService) Index(ctx context.Context) (responses.Categories, custom_error.CustomError) {
 
-	var response responses.Categories
 	categories, err := cs.repo.GetAll(ctx)
-
 	if err != nil {
-		if errors.Is(err, gorm.ErrRecordNotFound) {
-			return response, custom_error.New(err.Error(), custom_error.RecordNotFound, 404)
-		}
-		return response, custom_error.New(err.Error(), custom_error.InternalServerError, 500)
+		return responses.Categories{}, custom_error.HandleError(err, custom_error.RecordNotFound)
 	}
-
 	return responses.ToCategories(categories), custom_error.CustomError{}
 }
 
 func (cs CategoryService) Show(ctx context.Context, categoryID int) (responses.Category, custom_error.CustomError) {
-	var response responses.Category
 
 	cat, err := cs.repo.SelectBy(ctx, categoryID)
 	if err != nil {
-		if errors.Is(err, gorm.ErrRecordNotFound) {
-			return response, custom_error.New(err.Error(), custom_error.RecordNotFound, 404)
-		}
-		return response, custom_error.New(err.Error(), custom_error.InternalServerError, 500)
+		return responses.Category{}, custom_error.HandleError(err, custom_error.RecordNotFound)
 	}
-
 	return responses.ToCategory(cat), custom_error.CustomError{}
 }
 
@@ -58,8 +45,6 @@ func (cs CategoryService) CheckSlugUniqueness(ctx context.Context, slug string) 
 }
 
 func (cs CategoryService) Create(ctx context.Context, req requests.CreateCategoryRequest) (responses.Category, error) {
-
-	var response responses.Category
 
 	var cat = entities.Category{
 		Title: req.Title,
@@ -77,33 +62,25 @@ func (cs CategoryService) Create(ctx context.Context, req requests.CreateCategor
 	}
 	newCategory, err := cs.repo.Store(ctx, cat)
 	if err != nil {
-		return response, err
+		return responses.Category{}, err
 	}
-
 	return responses.ToCategory(newCategory), nil
 }
 
 func (cs CategoryService) GetAllCategories(ctx context.Context) (responses.Categories, custom_error.CustomError) {
-	var response responses.Categories
+
 	categories, err := cs.repo.GetAll(ctx)
-
 	if err != nil {
-		if errors.Is(err, gorm.ErrRecordNotFound) {
-			return response, custom_error.New(err.Error(), custom_error.RecordNotFound, 404)
-		}
-		return response, custom_error.New(err.Error(), custom_error.InternalServerError, 500)
+		return responses.Categories{}, custom_error.HandleError(err, custom_error.RecordNotFound)
 	}
-
 	return responses.ToCategories(categories), custom_error.CustomError{}
 }
+
 func (cs CategoryService) GetAllParentCategory(ctx context.Context) (responses.Categories, custom_error.CustomError) {
-	var response responses.Categories
+
 	categories, err := cs.repo.GetAllParent(ctx)
 	if err != nil {
-		if errors.Is(err, gorm.ErrRecordNotFound) {
-			return response, custom_error.New(err.Error(), custom_error.RecordNotFound, 404)
-		}
-		return response, custom_error.New(err.Error(), custom_error.InternalServerError, 500)
+		return responses.Categories{}, custom_error.HandleError(err, custom_error.RecordNotFound)
 	}
 	return responses.ToCategories(categories), custom_error.CustomError{}
 }
