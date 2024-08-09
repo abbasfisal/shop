e25c4a441ad7421e3ce8fa86e6d191de0335bfdc
abbas <abbasmomeny1994@gmail.com>
2024-08-09 11:20:58 +0330

    fix: seeder according to the new changes🤵

--- internal/database/mysql/seeder/seeder.go
+++ internal/database/mysql/seeder/seeder.go
@@ -13,6 +13,7 @@ func Seed() {
 
 	hashPass, _ := bcrypt.GenerateFromPassword([]byte("password"), bcrypt.DefaultCost)
 
+	//user
 	var user = entities.User{
 		FirstName:   "ali",
 		LastName:    "mohammadi",
@@ -21,8 +22,18 @@ func Seed() {
 		Type:        "admin",
 	}
 
-	var category = entities.Category{
+	//brand
+	var brand = entities.Brand{
+		Model:   gorm.Model{},
+		Title:   "zara",
+		Slug:    "zara",
+		Image:   "",
+		Product: nil,
+	}
 
+	//category
+	var category = entities.Category{
+		Model:    gorm.Model{},
 		Title:    "لباس و پوشاک",
 		Slug:     "clothes",
 		ParentID: nil,
@@ -45,21 +56,6 @@ func Seed() {
 						Image:         "men-t-shirt.jpg",
 						Status:        true,
 						SubCategories: nil,
-						Products: []entities.Product{
-							{
-								Model:         gorm.Model{},
-								CategoryID:    0,
-								Title:         "تیشرت مردانه مدل هایما",
-								Slug:          "hima-men-t-shirt",
-								Sku:           "sku-32932",
-								Status:        true,
-								OriginalPrice: 20000,
-								SalePrice:     250000,
-								Description:   "توضیحات تیشرت هایما",
-								Category:      entities.Category{},
-								ProductImages: nil,
-							},
-						},
 					},
 				},
 				Products: nil,
@@ -68,7 +64,8 @@ func Seed() {
 		Products: nil,
 	}
 
-	var attributes = []entities.Attribute{
+	//attribute -- attribute-values
+	var attributesAndValues = []entities.Attribute{
 		{
 			Model: gorm.Model{},
 			Title: "سایز",
@@ -132,8 +129,115 @@ func Seed() {
 		},
 	}
 
+	//attribute-value
+
+	//product
+	var product = entities.Product{
+		Model:         gorm.Model{},
+		BrandID:       1,
+		CategoryID:    3,
+		Title:         "تیشرت مردانه مدل هایما",
+		Slug:          "hima-men-t-shirt",
+		Sku:           "sku-32932",
+		Status:        true,
+		OriginalPrice: 20000,
+		SalePrice:     250000,
+		Description:   "توضیحات تیشرت هایما",
+	}
+
+	//product-attribute
+	var productAttribute = []entities.ProductAttribute{
+		{
+			Model:               gorm.Model{},
+			ProductID:           1,
+			AttributeID:         1, //size
+			AttributeTitle:      "سایز",
+			AttributeValueID:    1, //small
+			AttributeValueTitle: "s",
+		},
+		{
+			Model:               gorm.Model{},
+			ProductID:           1,
+			AttributeID:         1, //size
+			AttributeTitle:      "سایز",
+			AttributeValueID:    2, //medium
+			AttributeValueTitle: "m",
+		},
+		{
+			Model:               gorm.Model{},
+			ProductID:           1,
+			AttributeID:         2, //color
+			AttributeTitle:      "رنگ",
+			AttributeValueID:    6, //ابی
+			AttributeValueTitle: "ابی",
+		},
+		{
+			Model:               gorm.Model{},
+			ProductID:           1,
+			AttributeID:         2, //color
+			AttributeTitle:      "رنگ",
+			AttributeValueID:    7, //قرمز
+			AttributeValueTitle: "قرمز",
+		},
+	}
+
+	//product-inventory
+	var productInventory = []entities.ProductInventory{
+		{
+			Model:     gorm.Model{},
+			ProductID: 1,
+			Quantity:  20,
+		},
+		{
+			Model:     gorm.Model{},
+			ProductID: 1,
+			Quantity:  10,
+		},
+	}
+
+	//product-inventory-attribute
+	var productInventoryAttribute = []entities.ProductInventoryAttribute{
+		//1 : size : small
+		//2 : size : medium
+		//3 : color: blue
+		//4 : color: red
+
+		//small-blue = qty =20
+		{
+			Model:              gorm.Model{},
+			ProductID:          1,
+			ProductInventoryID: 1, //qty=20
+			ProductAttributeID: 1, //small
+		},
+		{
+			Model:              gorm.Model{},
+			ProductID:          1,
+			ProductInventoryID: 1, //qty=20
+			ProductAttributeID: 3, //blue
+		},
+
+		//medium-red = qty =10
+		{
+			Model:              gorm.Model{},
+			ProductID:          1,
+			ProductInventoryID: 1, //qty=10
+			ProductAttributeID: 2, //medium
+		},
+		{
+			Model:              gorm.Model{},
+			ProductID:          1,
+			ProductInventoryID: 1, //qty=10
+			ProductAttributeID: 4, //red
+		},
+	}
 	db.Create(&user)
 	db.Create(&category)
-	db.Create(&attributes)
+	db.Create(&brand)
+	db.Create(&attributesAndValues)
+	db.Create(&product)
+	db.Create(&productAttribute)
+	db.Create(&productInventory)
+	db.Create(&productInventoryAttribute)
+
 	fmt.Println("[Seed] tables successfully")
 }
