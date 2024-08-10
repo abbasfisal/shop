35f76a446ed78a381508a9697e8da3ce37b1cb97
abbas <abbasmomeny1994@gmail.com>
2024-08-10 08:28:33 +0330

    add: response product_inventory_attribute_resposne 👋 to product_response

--- internal/modules/admin/responses/product_response.go
+++ internal/modules/admin/responses/product_response.go
@@ -3,23 +3,24 @@ package responses
 import "shop/internal/entities"
 
 type Product struct {
-	ID         uint
-	CategoryID uint
-	BrandID    uint
-	Title      string
-	Slug       string
-	Sku        string
-	Status     bool
-	//Quantity      uint
-	OriginalPrice     uint
-	SalePrice         uint
-	Description       string
-	Category          Category
-	Brand             Brand
-	Images            ImageProducts
-	ProductAttributes ProductAttributes
+	ID            uint
+	CategoryID    uint
+	BrandID       uint
+	Title         string
+	Slug          string
+	Sku           string
+	Status        bool
+	OriginalPrice uint
+	SalePrice     uint
+	Description   string
 
-	ProductInventories ProductInventories
+	//relation
+	Category                   Category
+	Brand                      Brand
+	Images                     ImageProducts
+	ProductAttributes          ProductAttributes
+	ProductInventories         ProductInventories
+	ProductInventoryAttributes ProductInventoryAttributes
 }
 
 type Products struct {
@@ -35,22 +36,23 @@ func ToProducts(products []entities.Product) Products {
 }
 func ToProduct(p entities.Product) Product {
 	return Product{
-		ID:         p.ID,
-		CategoryID: p.CategoryID,
-		BrandID:    p.BrandID,
-		Title:      p.Title,
-		Slug:       p.Slug,
-		Sku:        p.Sku,
-		Status:     p.Status,
+		ID:            p.ID,
+		CategoryID:    p.CategoryID,
+		BrandID:       p.BrandID,
+		Title:         p.Title,
+		Slug:          p.Slug,
+		Sku:           p.Sku,
+		Status:        p.Status,
+		OriginalPrice: p.OriginalPrice,
+		SalePrice:     p.SalePrice,
+		Description:   p.Description,
 
-		OriginalPrice:     p.OriginalPrice,
-		SalePrice:         p.SalePrice,
-		Description:       p.Description,
-		Category:          ToCategory(p.Category),
-		Brand:             ToBrand(p.Brand),
-		Images:            ToImageProducts(p.ProductImages),
-		ProductAttributes: ToProductAttributes(p.ProductAttributes),
-
-		ProductInventories: ToProductInventories(p.ProductInventories),
+		//relation
+		Category:                   ToCategory(p.Category),
+		Brand:                      ToBrand(p.Brand),
+		Images:                     ToImageProducts(p.ProductImages),
+		ProductAttributes:          ToProductAttributes(p.ProductAttributes),
+		ProductInventories:         ToProductInventories(p.ProductInventories),
+		ProductInventoryAttributes: ToProductInventoryAttributes(p.ProductInventoryAttributes),
 	}
 }
