00e0715c64ec577aaf608fbc70ac1bf851595b00
abbas <abbasmomeny1994@gmail.com>
2024-08-10 08:27:52 +0330

    add: response product_inventory_attribute_resposne 👋

---
+++ internal/modules/admin/responses/product_inventory_attribute_resposne.go
@@ -0,0 +1,39 @@
+package responses
+
+import (
+	"shop/internal/entities"
+)
+
+type ProductInventoryAttribute struct {
+	ID                 uint
+	ProductID          uint
+	ProductInventoryID uint
+	ProductAttributeID uint
+
+	//
+	ProductAttribute ProductAttribute
+}
+type ProductInventoryAttributes struct {
+	Data []ProductInventoryAttribute
+}
+
+func ToProductInventoryAttribute(pi entities.ProductInventoryAttribute) ProductInventoryAttribute {
+	return ProductInventoryAttribute{
+		ID:        pi.ID,
+		ProductID: pi.ProductID,
+
+		ProductInventoryID: pi.ProductInventoryID,
+		ProductAttributeID: pi.ProductAttributeID,
+
+		//
+		ProductAttribute: ToProductAttribute(pi.ProductAttribute),
+	}
+}
+
+func ToProductInventoryAttributes(pis []entities.ProductInventoryAttribute) ProductInventoryAttributes {
+	var pResponse ProductInventoryAttributes
+	for _, p := range pis {
+		pResponse.Data = append(pResponse.Data, ToProductInventoryAttribute(p))
+	}
+	return pResponse
+}
