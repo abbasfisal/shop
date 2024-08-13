555d906f69285fca1879ada78602db17bde9fa6f
abbas <abbasmomeny1994@gmail.com>
2024-08-13 20:55:04 +0330

    add : customer entity 🐱

---
+++ internal/entities/customer.go
@@ -0,0 +1,16 @@
+package entities
+
+import "gorm.io/gorm"
+
+type Customer struct {
+	gorm.Model
+	Mobile    string `gorm:"index:unique"`
+	FirstName string
+	LastName  string
+	Active    bool
+	//Gender    bool //true:male , false:female
+	//DateOfBirth
+
+	//--------- Relation
+	Address Address `gorm:"foreignKey:CustomerID"`
+}
