acbb50286639251ec0ac93e1cfa73d52e0e64f77
abbas <abbasmomeny1994@gmail.com>
2024-08-13 20:55:20 +0330

    update : entity address

--- internal/entities/address.go
+++ internal/entities/address.go
@@ -4,10 +4,21 @@ import "gorm.io/gorm"
 
 type Address struct {
 	gorm.Model
-	UserID      uint
-	Floor       uint8 `gorm:"not null"`
-	Number      uint8
-	Phase       string `gorm:"type:varchar(50)"`
-	Block       string `gorm:"type:varchar(50)"`
+	CustomerID uint
+
+	ZipCode string
+	Unit    string
+	Floor   uint8 `gorm:"not null"`
+	Number  string
+
+	Latitude  float64
+	Longitude float64
+
+	//Phase       string `gorm:"type:varchar(50)"`
+	//Block       string `gorm:"type:varchar(50)"`
+
 	Description string `gorm:"type:varchar(255)"`
+
+	//------ Relation
+
 }
