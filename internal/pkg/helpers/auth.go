abebbda3c982c5f88dfcdb3a1f9f0fda75c8c945
abbas <abbasmomeny1994@gmail.com>
2024-08-16 23:32:16 +0330

    ref : CustomerAuth() in helpers

--- internal/pkg/helpers/auth.go
+++ internal/pkg/helpers/auth.go
@@ -6,6 +6,8 @@ import (
 	"shop/internal/database/mysql"
 	"shop/internal/entities"
 	adminAuthRepo "shop/internal/modules/admin/repositories/auth"
+	customerAuthRepo "shop/internal/modules/public/repositories/customer_auth"
+	customerResponse "shop/internal/modules/public/responses"
 	"shop/internal/pkg/sessions"
 	"strconv"
 )
@@ -24,18 +26,18 @@ func Auth(c *gin.Context) entities.User {
 	return user
 }
 
-func CustomerAuth(c *gin.Context) entities.Customer {
-	//authID := sessions.GET(c, "auth_id")
-	//fmt.Println("authid:", authID)
-	//if authID == "" {
-	//	return entities.User{}
-	//}
-	//
-	//repo := adminAuthRepo.NewAuthenticateRepository(mysql.Get())
-	//userID, _ := strconv.Atoi(authID)
-	//user, _ := repo.FindByUserID(c, uint(userID))
-	//
-	//return user
+func CustomerAuth(c *gin.Context) customerResponse.Customer {
+	sessionID := sessions.GET(c, "session_id") //session uuid
+	fmt.Println("-- helpers/auth.go/CustomerAuth  ---  customer sessionID [uuid ]:", sessionID)
+	if sessionID == "" {
+		return customerResponse.Customer{}
+	}
 
-	return entities.Customer{}
+	repo := customerAuthRepo.NewAuthenticateRepository(mysql.Get())
+	customer, err := repo.FindCustomerBySessionID(c, sessionID)
+	if err != nil {
+		return customerResponse.Customer{}
+	}
+
+	return customerResponse.ToCustomer(customer)
 }
