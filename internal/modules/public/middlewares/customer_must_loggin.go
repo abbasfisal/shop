7a6f6fe179fcfa3f49ed56a93adb2eb0b5fe3319
abbas <abbasmomeny1994@gmail.com>
2024-08-18 20:45:23 +0330

    add: middleware to check customer have to loggedIn

---
+++ internal/modules/public/middlewares/customer_must_loggin.go
@@ -0,0 +1,25 @@
+package middlewares
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"shop/internal/pkg/helpers"
+)
+
+// CustomerMustLogin  this middleware just check is customer LoggedIn if not redirect
+func CustomerMustLogin() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		fmt.Println("Middleware fired : customerMustBelogin")
+		customer := helpers.CustomerAuth(c)
+
+		if customer.ID <= 0 {
+			c.Redirect(http.StatusFound, "/hihiih")
+			c.Abort()
+			return
+		}
+
+		c.Next()
+	}
+}
