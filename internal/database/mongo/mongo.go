118d7dfed8305c8dd82d8773e96f36ae0e808961
abbas <abbasmomeny1994@gmail.com>
2024-09-06 08:57:25 +0330

    fix: use global client var replace 	client, err := mongo.Connect(context.TODO(), clientOptions) with 	client, err = mongo.Connect(context.TODO(), clientOptions) 🤘🤙

--- internal/database/mongo/mongo.go
+++ internal/database/mongo/mongo.go
@@ -11,26 +11,39 @@ import (
 
 var client *mongo.Client
 
-func Connect() {
+func Connect() error {
 
+	var err error
 	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017/")
-	client, err := mongo.Connect(context.TODO(), clientOptions)
+	client, err = mongo.Connect(context.TODO(), clientOptions)
 	if err != nil {
-		log.Fatal("mongo error creating client: ", err)
+		log.Println("mongo error creating client: ", err)
+		return err
 	}
 
 	err = client.Ping(context.TODO(), nil)
 	if err != nil {
-		log.Fatal("mongo error pinging database: ", err)
+		log.Println("mongo error pinging database: ", err)
+		return err
 	}
 
 	fmt.Println("\n[mongo] connected to MongoDB successfully")
-
+	return nil
 }
 func Get() *mongo.Client {
 	return client
 }
 
 func GetCollection(CollectionName string) *mongo.Collection {
-	return client.Database(viper.GetString("mongodb.name")).Collection(CollectionName)
+	if client == nil {
+		log.Fatal("MongoDB client is not initialized. Did you call Connect?")
+	}
+
+	// Get the database name from viper and return the collection
+	dbName := viper.GetString("mongodb.name")
+	if dbName == "" {
+		log.Fatal("MongoDB database name not found in config")
+	}
+
+	return client.Database(dbName).Collection(CollectionName)
 }
