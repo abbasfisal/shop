4f829ce77a1abb93bed3b2945c7e03708a24330e
abbas <abbasmomeny1994@gmail.com>
2024-09-23 20:41:04 +0330

    add menu response

---
+++ internal/modules/public/responses/menu_response.go
@@ -0,0 +1,33 @@
+package responses
+
+import (
+	"shop/internal/entities"
+)
+
+type CategoryResponse struct {
+	ID       uint
+	Priority *uint
+	Title    string
+	Slug     string
+	ParentID *uint
+	Status   bool
+
+	SubCategories []CategoryResponse `json:"SubCategories,omitempty"`
+}
+
+func ToMenuResponse(category entities.Category) CategoryResponse {
+	var subCategories []CategoryResponse
+	for _, subCategory := range category.SubCategories {
+		subCategories = append(subCategories, ToMenuResponse(subCategory))
+	}
+
+	return CategoryResponse{
+		ID:            category.ID,
+		Priority:      category.Priority,
+		Title:         category.Title,
+		Slug:          category.Slug,
+		ParentID:      category.ParentID,
+		Status:        category.Status,
+		SubCategories: subCategories,
+	}
+}
