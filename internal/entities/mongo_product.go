26e5b34d19cb9836a99d37a734982715a36c84a6
abbas <abbasmomeny1994@gmail.com>
2024-09-30 13:35:10 +0330

    edit mongo_product entity

--- internal/entities/mongo_product.go
+++ internal/entities/mongo_product.go
@@ -1,23 +1,81 @@
 package entities
 
-import (
-	"time"
-)
+import "time"
 
 type MongoProduct struct {
-	ID            uint     `bson:"id"`
-	CategoryID    uint     `bson:"category_id"`
-	BrandID       uint     `bson:"brand_id"`
-	Title         string   `bson:"title"`
-	Slug          string   `bson:"slug"`
-	Sku           string   `bson:"sku"`
-	Status        bool     `bson:"status"`
-	OriginalPrice uint     `bson:"original_price"`
-	SalePrice     uint     `bson:"sale_price"`
-	Description   string   `bson:"description"`
-	Images        []string `bson:"images"`
+	Product     P                    `bson:"product"`
+	Inventories map[string]Inventory `bson:"inventories"`
+}
+
+type P struct {
+	ID int64 `bson:"id"`
+
+	Category   C     `bson:"Category"`
+	CategoryID int64 `bson:"category_id"`
+
+	Brand   B     `bson:"Brand"`
+	BrandID int64 `bson:"brand_id"`
+
+	Title         string `bson:"title"`
+	Slug          string `bson:"slug"`
+	Sku           string `bson:"sku"`
+	Status        bool   `bson:"status"`
+	OriginalPrice int64  `bson:"original_price"`
+	SalePrice     int64  `bson:"sale_price"`
+	Discount      int64  `bson:"Discount"`
+	Description   string `bson:"description"`
+
+	//Images []string `bson:"images"`
+	Images   Img `bson:"Images"`
+	Features F   `bson:"Features"`
 
 	CreatedAt time.Time `bson:"created_at"`
 	UpdatedAt time.Time `bson:"updated_at"`
-	DeletedAt time.Time `bson:"deleted_at"`
+}
+
+type C struct {
+	ID       int64  `bson:"id"`
+	ParentID int64  `bson:"parent_id"`
+	Title    string `bson:"title"`
+	Slug     string `bson:"slug"`
+}
+
+type B struct {
+	ID    int64  `bson:"id"`
+	Title string `bson:"title"`
+	Slug  string `bson:"slug"`
+}
+
+type Img struct {
+	Data []ImgData `bson:"Data"`
+}
+
+type ImgData struct {
+	ID           int64  `bson:"ID"` //todo:correct capital
+	OriginalPath string `bson:"OriginalPath"`
+	FullPath     string `bson:"FullPath"`
+}
+
+type F struct {
+	Data []FData `bson:"Data"`
+}
+
+type FData struct {
+	ID        int64  `bson:"ID"`
+	ProductID int64  `bson:"ProductID"`
+	Title     string `bson:"Title"`
+	Value     string `bson:"Value"`
+}
+
+type Inventory struct {
+	InventoryID int64                 `bson:"inventory_id"`
+	Quantity    int64                 `bson:"quantity"`
+	Attributes  []InventoryAttributes `bson:"attributes"`
+}
+type InventoryAttributes struct {
+	AttributeID                 int64  `bson:"attribute_id"`
+	AttributeTitle              string `bson:"attribute_title"`
+	AttributeValueID            int64  `bson:"attribute_value_id"`
+	AttributeValueTitle         string `bson:"attribute_value_title"`
+	ProductInventoryAttributeID int64  `bson:"product_inventory_attribute_id"`
 }
