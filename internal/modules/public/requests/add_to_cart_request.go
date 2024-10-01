e36310b92b4c9a1dfe2f7e8065d039b85e6ef5d2
abbas <abbasmomeny1994@gmail.com>
2024-10-01 20:41:20 +0330

    ref: remove pointer inventory_id in add_to_cart_request

--- internal/modules/public/requests/add_to_cart_request.go
+++ internal/modules/public/requests/add_to_cart_request.go
@@ -1,6 +1,6 @@
 package requests
 
 type AddToCartRequest struct {
-	ProductID   string `form:"product_id" binding:"required"`
-	InventoryID *uint  `form:"inventory_id"`
+	ProductID   string `form:"product_id" binding:"required"` //ProductID is a ObjectID in mongo document
+	InventoryID uint   `form:"inventory_id"`
 }
