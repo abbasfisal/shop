63fffe05aa9135563034f134bf3d3ac406dcf693
abbas <abbasmomeny1994@gmail.com>
2024-10-01 20:39:44 +0330

    add GetProductByObjectID() to get product from mongo by objectID

--- internal/modules/public/repositories/home_mongo/mongo_repository.go
+++ internal/modules/public/repositories/home_mongo/mongo_repository.go
@@ -5,9 +5,11 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"shop/internal/database/mongodb"
 	"shop/internal/entities"
+	"shop/internal/modules/public/requests"
 	"shop/internal/modules/public/responses"
 	"shop/internal/pkg/custom_error"
 )
@@ -41,4 +43,28 @@ func (m MongoHomeRepository) GetProduct(c *gin.Context, productSku string, produ
 	fmt.Printf("%+v", mongoProduct)
 	return responses.ToMongoProductResponse(mongoProduct), nil
 
+}
+
+func (m MongoHomeRepository) GetProductByObjectID(c *gin.Context, productObjectID primitive.ObjectID, req requests.AddToCartRequest) (entities.MongoProduct, error) {
+	productionCollection := mongodb.GetCollection(mongodb.ProductsCollection)
+	filter := bson.D{
+		{"_id", productObjectID},
+	}
+	var mongoProduct entities.MongoProduct
+	FindProductErr := productionCollection.FindOne(c, filter).Decode(&mongoProduct)
+
+	if FindProductErr != nil {
+		if FindProductErr == mongo.ErrNoDocuments {
+			// محصولی یافت نشد
+			fmt.Println("[Error Not Found Document ]-[GetProductByObjectID]:err:", custom_error.RecordNotFound)
+			return mongoProduct, errors.New(custom_error.RecordNotFound)
+		}
+		fmt.Println("[Error]-[GetProductByObjectID]:err:", custom_error.SomethingWrongHappened)
+
+		return mongoProduct, errors.New(custom_error.SomethingWrongHappened)
+	}
+
+	fmt.Println("[success]-[GetProductByObjectID]-msg:record found")
+
+	return mongoProduct, nil
 }
--- internal/modules/public/repositories/home_mongo/mongo_repository_interface.go
+++ internal/modules/public/repositories/home_mongo/mongo_repository_interface.go
@@ -1,7 +1,13 @@
 package home_mongo
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"shop/internal/entities"
+	"shop/internal/modules/public/requests"
+)
 
 type MongoHomeRepositoryInterface interface {
 	GetProduct(c *gin.Context, productSku string, productSlug string) (map[string]interface{}, error)
+	GetProductByObjectID(c *gin.Context, productObjectID primitive.ObjectID, req requests.AddToCartRequest) (entities.MongoProduct, error)
 }
