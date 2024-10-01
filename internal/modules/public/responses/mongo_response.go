31f48934d15f51e84c275014a58a57b50c6d5f01
abbas <abbasmomeny1994@gmail.com>
2024-10-01 10:07:31 +0330

    add _id to mongo response

--- internal/modules/public/responses/mongo_response.go
+++ internal/modules/public/responses/mongo_response.go
@@ -4,6 +4,7 @@ import "shop/internal/entities"
 
 func ToMongoProductResponse(mongoProduct entities.MongoProduct) map[string]interface{} {
 	return map[string]interface{}{
+		"_id":         mongoProduct.ID.Hex(),
 		"product":     mongoProduct.Product,
 		"inventories": mongoProduct.Inventories,
 	}
