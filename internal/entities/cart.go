772a0c6034ec2d07a57908e39cf332a15c7ce09e
abbas <abbasmomeny1994@gmail.com>
2024-10-01 20:40:38 +0330

    ref: cart entity

--- internal/entities/cart.go
+++ internal/entities/cart.go
@@ -4,11 +4,28 @@ import "gorm.io/gorm"
 
 type Cart struct {
 	gorm.Model
-	UserID    uint
-	ProductID uint
+	UserID      uint
+	ProductID   uint
+	InventoryID uint
 
 	Count  uint8 `gorm:"type:smallInt"`
 	Status uint8
 
+	//--
+	ProductTitle string
+	ProductImage string
+	ProductSlug  string
+
+	OriginalPrice uint
+	SalePrice     uint
+
+	//----
+
 	Product Product
 }
+
+//user1
+//product1
+//inventory1
+//count2
+//
