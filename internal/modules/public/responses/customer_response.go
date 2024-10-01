5ea080030b198d5d343fae14a7fa79396ebb3ef1
abbas <abbasmomeny1994@gmail.com>
2024-10-01 23:55:14 +0330

    add: carts to customer response

--- internal/modules/public/responses/customer_response.go
+++ internal/modules/public/responses/customer_response.go
@@ -11,7 +11,44 @@ type Customer struct {
 	LastName  string
 	IsActive  bool
 	//address
+	Carts []Cart
+}
+type Cart struct {
+	ID            uint
+	CustomerID    uint
+	ProductID     uint
+	InventoryID   uint
+	Count         uint8
+	Status        uint8
+	ProductTitle  string
+	ProductSlug   string
+	ProductImage  string
+	OriginalPrice uint
+	SalePrice     uint
+}
 
+func toCart(cartItem entities.Cart) Cart {
+	return Cart{
+		ID:            cartItem.ID,
+		CustomerID:    cartItem.CustomerID,
+		ProductID:     cartItem.ProductID,
+		InventoryID:   cartItem.InventoryID,
+		Count:         cartItem.Count,
+		Status:        cartItem.Status,
+		ProductTitle:  cartItem.ProductTitle,
+		ProductSlug:   cartItem.ProductSlug,
+		ProductImage:  cartItem.ProductImage,
+		OriginalPrice: cartItem.OriginalPrice,
+		SalePrice:     cartItem.SalePrice,
+	}
+}
+
+func toCarts(cartData []entities.Cart) []Cart {
+	var carts []Cart
+	for _, item := range cartData {
+		carts = append(carts, toCart(item))
+	}
+	return carts
 }
 
 func ToCustomer(cus entities.Customer) Customer {
@@ -22,6 +59,7 @@ func ToCustomer(cus entities.Customer) Customer {
 		FirstName: cus.FirstName,
 		LastName:  cus.LastName,
 		IsActive:  cus.Active,
+		Carts:     toCarts(cus.Carts),
 	}
 
 }
