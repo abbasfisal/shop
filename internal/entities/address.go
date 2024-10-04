a087db94efc1ec89d5e19cef1ef813b0a063aebd
abbas <abbasmomeny1994@gmail.com>
2024-10-04 23:16:21 +0330

    change: entity address fields

--- internal/entities/address.go
+++ internal/entities/address.go
@@ -4,20 +4,11 @@ import "gorm.io/gorm"
 
 type Address struct {
 	gorm.Model
-	CustomerID uint
-
-	ZipCode string
-	Unit    string
-	Floor   uint8 `gorm:"not null"`
-	Number  string
-
-	Latitude  float64
-	Longitude float64
-
-	//Phase       string `gorm:"type:varchar(50)"`
-	//Block       string `gorm:"type:varchar(50)"`
-
-	Description string `gorm:"type:varchar(255)"`
+	CustomerID         uint
+	ReceiverName       string
+	ReceiverMobile     string
+	ReceiverAddress    string
+	ReceiverPostalCode string
 
 	//------ Relation
 
