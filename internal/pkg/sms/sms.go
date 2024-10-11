ec9196c1fb4bc3a6cb360309be9695e54231aa0a
abbas <abbasmomeny1994@gmail.com>
2024-10-11 18:39:31 +0330

    add: sms

---
+++ internal/pkg/sms/sms.go
@@ -0,0 +1,100 @@
+package sms
+
+import (
+	"fmt"
+	"github.com/kavenegar/kavenegar-go"
+)
+
+type Service interface {
+	Send(receptors []string, message string)
+	SendOTP(receptor string, token string)
+}
+
+type ServiceManager struct {
+	Service Service
+}
+
+func (sm *ServiceManager) SetService(service Service) {
+	sm.Service = service
+}
+
+func (sm *ServiceManager) Send(receptors []string, message string) {
+	if sm.Service == nil {
+		return
+	}
+	sm.Service.Send(receptors, message)
+}
+func (sm *ServiceManager) SendOTP(receptor string, token string) {
+	if sm.Service == nil {
+		return
+	}
+	sm.Service.SendOTP(receptor, token)
+}
+
+func GetSMSManager() *ServiceManager {
+	return &ServiceManager{}
+}
+
+func Send(receptors []string, message string) {
+	GetSMSManager().Send(receptors, message)
+}
+
+/**
+ *---------------------------
+ *			KAVENEGAR
+ *---------------------------
+ */
+
+type KaveNegar struct {
+	ApiKey string
+}
+
+func NewKaveNegar(apiKey string) *KaveNegar {
+	return &KaveNegar{ApiKey: apiKey}
+}
+
+func (k *KaveNegar) Send(receptors []string, message string) {
+	api := kavenegar.New(" your apikey ")
+	sender := ""
+
+	if res, err := api.Message.Send(sender, receptors, message, nil); err != nil {
+		switch err := err.(type) {
+		case *kavenegar.APIError:
+			fmt.Println(err.Error())
+			break
+		case *kavenegar.HTTPError:
+			fmt.Println(err.Error())
+			break
+		default:
+			fmt.Println(err.Error())
+			break
+		}
+	} else {
+		for _, r := range res {
+			fmt.Println("MessageID 	= ", r.MessageID)
+			fmt.Println("Status    	= ", r.Status)
+		}
+	}
+}
+
+func (k *KaveNegar) SendOTP(receptor string, token string) {
+
+	api := kavenegar.New(k.ApiKey)
+	template := "" //load from config
+	params := &kavenegar.VerifyLookupParam{}
+
+	if res, err := api.Verify.Lookup(receptor, template, token, params); err != nil {
+		switch err := err.(type) {
+		case *kavenegar.APIError:
+			fmt.Println(err.Error())
+		case *kavenegar.HTTPError:
+			fmt.Println(err.Error())
+		default:
+			fmt.Println(err.Error())
+		}
+	} else {
+		fmt.Println("MessageID 	= ", res.MessageID)
+		fmt.Println("Status    	= ", res.Status)
+	}
+
+}
