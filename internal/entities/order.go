3eebc9345b87c581a82b10f230fa23eda4353fd2
abbas <abbasmomeny1994@gmail.com>
2024-11-26 20:09:27 +0330

    add: order status , payment status const

--- internal/entities/order.go
+++ internal/entities/order.go
@@ -17,3 +17,18 @@ type Order struct {
 	OrderItems []OrderItem `gorm:"foreignKey:OrderID;constraint:OnDelete:CASCADE;"`
 	Payment    Payment     `gorm:"foreignKey:OrderID"`
 }
+
+// Order Status Constants
+const (
+	OrderPending     uint = 1  // در حال بررسی
+	OrderConfirmed   uint = 2  // تایید شده
+	OrderCancelled   uint = 3  // لغو شده
+	OrderPreparing   uint = 4  // در حال آماده‌سازی
+	OrderReadyToShip uint = 5  // آماده برای ارسال
+	OrderShipped     uint = 6  // ارسال شده
+	OrderInTransit   uint = 7  // در مسیر ارسال
+	OrderDelivered   uint = 8  // تحویل داده شده
+	OrderReturned    uint = 9  // مرجوع شده
+	OrderCompleted   uint = 10 // تکمیل شده
+	OrderUnderReview uint = 11 // اختلاف یا مشکل
+)
--- internal/entities/payment.go
+++ internal/entities/payment.go
@@ -17,5 +17,12 @@ type Payment struct {
 	Status      int //payment status -> 0-pending,1-paid ,2-failed
 
 	//-- relation
-
 }
+
+// Payment Status Constants
+const (
+	PaymentPending uint = 1 // منتظر پرداخت
+	PaymentSuccess uint = 2 // پرداخت موفق
+	PaymentFailed  uint = 3 // پرداخت ناموفق
+	PaymentRetry   uint = 4 // در انتظار پرداخت مجدد
+)
