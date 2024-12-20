50070e7de9a23b495ccd99954ff2c7a3fbd6a834
abbas <abbasmomeny1994@gmail.com>
2024-12-20 17:46:31 +0330

    add: schedule server

---
+++ cmd/job/scheduler/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"github.com/hibiken/asynq"
+	"os"
+
+	"log"
+	"shop/internal/modules/public/jobs"
+)
+
+func main() {
+
+	schedule := asynq.NewScheduler(asynq.RedisClientOpt{Addr: fmt.Sprintf("%s:%s", os.Getenv("REDIS_DB"), os.Getenv("REDIS_PORT"))}, &asynq.SchedulerOpts{})
+
+	registerSchedules(schedule)
+
+	err := schedule.Start()
+	if err != nil {
+		log.Fatal("[x] error start scheduler :", err)
+	}
+
+}
+
+func registerSchedules(schedule *asynq.Scheduler) {
+
+	//---------------------- example task
+	exampleTask, exampleTaskErr := jobs.TaskExample("your data ")
+	if exampleTaskErr == nil {
+		_, err := schedule.Register("@every 3s", exampleTask)
+		if err != nil {
+			log.Println("[x] error `exampleTaskErr` run schedule:", err)
+			return
+		}
+	}
+	//------------------------
+}
