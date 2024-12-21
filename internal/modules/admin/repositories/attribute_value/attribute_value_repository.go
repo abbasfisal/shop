6b3fca242abcd6501fa5c1970c18148978a341b3
abbas <abbasmomeny1994@gmail.com>
2024-12-21 16:57:25 +0330

    ref: return pointer instead returning value attribute_value repo,interface

--- internal/modules/admin/repositories/attribute_value/attribute_value_repository.go
+++ internal/modules/admin/repositories/attribute_value/attribute_value_repository.go
@@ -13,16 +13,16 @@ type AttributeValueRepository struct {
 	db *gorm.DB
 }
 
-func NewAttributeRepository(db *gorm.DB) AttributeValueRepository {
-	return AttributeValueRepository{db: db}
+func NewAttributeRepository(db *gorm.DB) AttributeValueRepositoryInterface {
+	return &AttributeValueRepository{db: db}
 }
 
-func (ar AttributeValueRepository) Store(ctx context.Context, attr entities.AttributeValue) (entities.AttributeValue, error) {
+func (ar *AttributeValueRepository) Store(ctx context.Context, attr *entities.AttributeValue) (*entities.AttributeValue, error) {
 
 	var attribute entities.Attribute
-	err := ar.db.First(&attribute, attr.AttributeID).Error
+	err := ar.db.WithContext(ctx).First(&attribute, attr.AttributeID).Error
 	if err != nil {
-		return entities.AttributeValue{}, err
+		return nil, err
 	}
 
 	attr.AttributeTitle = attribute.Title
@@ -31,25 +31,25 @@ func (ar AttributeValueRepository) Store(ctx context.Context, attr entities.Attr
 	return attr, attErr
 }
 
-func (ar AttributeValueRepository) GetAllAttribute(c *gin.Context) ([]entities.Attribute, error) {
-	var attributes []entities.Attribute
+func (ar *AttributeValueRepository) GetAllAttribute(c *gin.Context) ([]*entities.Attribute, error) {
+	var attributes []*entities.Attribute
 	err := ar.db.WithContext(c).Preload("AttributeValues").Find(&attributes).Error
 	return attributes, err
 }
 
-func (ar AttributeValueRepository) Find(c *gin.Context, attributeValueID int) (entities.AttributeValue, error) {
+func (ar *AttributeValueRepository) Find(c *gin.Context, attributeValueID int) (*entities.AttributeValue, error) {
 	var attValue entities.AttributeValue
 	err := ar.db.First(&attValue, attributeValueID).Error
 
-	return attValue, err
+	return &attValue, err
 }
 
-func (ar AttributeValueRepository) Update(c *gin.Context, attributeValueID int, req requests.UpdateAttributeValueRequest) (entities.AttributeValue, error) {
+func (ar *AttributeValueRepository) Update(c *gin.Context, attributeValueID int, req *requests.UpdateAttributeValueRequest) (*entities.AttributeValue, error) {
 	var attributeValue entities.AttributeValue
 
 	err := ar.db.WithContext(c).Preload("Attribute").First(&attributeValue, attributeValueID).Error
 	if err != nil {
-		return attributeValue, err
+		return nil, err
 	}
 
 	updateErr := ar.db.
@@ -60,8 +60,8 @@ func (ar AttributeValueRepository) Update(c *gin.Context, attributeValueID int, 
 		Error
 
 	if updateErr != nil {
-		return entities.AttributeValue{}, updateErr
+		return nil, updateErr
 	}
 
-	return attributeValue, nil
+	return &attributeValue, nil
 }
--- internal/modules/admin/repositories/attribute_value/attribute_value_repository_interface.go
+++ internal/modules/admin/repositories/attribute_value/attribute_value_repository_interface.go
@@ -8,8 +8,8 @@ import (
 )
 
 type AttributeValueRepositoryInterface interface {
-	Store(ctx context.Context, attr entities.AttributeValue) (entities.AttributeValue, error)
-	GetAllAttribute(c *gin.Context) ([]entities.Attribute, error)
-	Find(c *gin.Context, attributeValueID int) (entities.AttributeValue, error)
-	Update(c *gin.Context, attributeValueID int, req requests.UpdateAttributeValueRequest) (entities.AttributeValue, error)
+	Store(ctx context.Context, attr *entities.AttributeValue) (*entities.AttributeValue, error)
+	GetAllAttribute(c *gin.Context) ([]*entities.Attribute, error)
+	Find(c *gin.Context, attributeValueID int) (*entities.AttributeValue, error)
+	Update(c *gin.Context, attributeValueID int, req *requests.UpdateAttributeValueRequest) (*entities.AttributeValue, error)
 }
