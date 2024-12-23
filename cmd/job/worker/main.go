d50dd020eba164a39fd0499db83657f3a8292ea3
abbas <abbasmomeny1994@gmail.com>
2024-12-23 17:23:14 +0330

    ref: load dependencies in worker.go

--- cmd/job/worker/main.go
+++ cmd/job/worker/main.go
@@ -5,26 +5,40 @@ import (
 	"github.com/hibiken/asynq"
 	"log"
 	"os"
-	publicJob "shop/internal/modules/public/jobs"
+	adminJob "shop/internal/modules/admin/jobs"
+	"shop/internal/pkg/bootstrap"
 )
 
 func main() {
+	dep, err := bootstrap.Initialize()
+	if err != nil {
+		log.Fatalf("[x] failed to initialize dependencies: %v", err)
+	}
+
 	server := asynq.
 		NewServer(
-			asynq.RedisClientOpt{Addr: fmt.Sprintf("%s:%s", os.Getenv("REDIS_DB"))},
+			asynq.RedisClientOpt{Addr: fmt.Sprintf("%s:%s", os.Getenv("REDIS_HOSTNAME"), os.Getenv("REDIS_PORT"))},
 			asynq.Config{
 				Concurrency: 10,
 				//Queues:                   nil,
 			},
 		)
 
+	errs := server.Ping()
+	if errs != nil {
+		log.Fatal("------[ping err:]", errs)
+		return
+	}
+
 	//mux server
 	mux := asynq.NewServeMux()
-	mux.HandleFunc(publicJob.TypeSendWelcomeSMS, publicJob.HandleTaskSendWelcomeSMS)
-	mux.HandleFunc(publicJob.TypeExample, publicJob.HandleExampleTask)
+	//mux.HandleFunc(publicJob.TypeSendWelcomeSMS, publicJob.HandleTaskSendWelcomeSMS)
+	//mux.HandleFunc(publicJob.TypeExample, publicJob.HandleExampleTask)
+
+	mux.Handle(adminJob.CancelPendingOrders, adminJob.NewCancelJob(dep))
 
 	//>>>>> run serve r<<<<<
-	err := server.Run(mux)
+	err = server.Run(mux)
 	if err != nil {
 		log.Fatal("[x] job worker start failed:", err)
 	}
