b1a9d4bef37b903bf7d2b7c6cbe5a5ba092d92be
abbas <abbasmomeny1994@gmail.com>
2024-12-23 17:21:03 +0330

    add: register TaskCancelPendingOrders in scheduler

--- cmd/job/scheduler/main.go
+++ cmd/job/scheduler/main.go
@@ -4,18 +4,18 @@ import (
 	"fmt"
 	"github.com/hibiken/asynq"
 	"os"
+	adminJob "shop/internal/modules/admin/jobs"
 
 	"log"
-	"shop/internal/modules/public/jobs"
 )
 
 func main() {
 
-	schedule := asynq.NewScheduler(asynq.RedisClientOpt{Addr: fmt.Sprintf("%s:%s", os.Getenv("REDIS_DB"), os.Getenv("REDIS_PORT"))}, &asynq.SchedulerOpts{})
+	schedule := asynq.NewScheduler(asynq.RedisClientOpt{Addr: fmt.Sprintf("%s:%s", os.Getenv("REDIS_HOSTNAME"), os.Getenv("REDIS_PORT"))}, &asynq.SchedulerOpts{})
 
 	registerSchedules(schedule)
 
-	err := schedule.Start()
+	err := schedule.Run()
 	if err != nil {
 		log.Fatal("[x] error start scheduler :", err)
 	}
@@ -25,14 +25,20 @@ func main() {
 // registerSchedules is responsible to register our tasks with specific cronspec
 func registerSchedules(schedule *asynq.Scheduler) {
 
-	//---------------------- example task
-	exampleTask, exampleTaskErr := jobs.TaskExample("your data ")
-	if exampleTaskErr == nil {
-		_, err := schedule.Register("@every 3s", exampleTask)
-		if err != nil {
-			log.Println("[x] error `exampleTaskErr` run schedule:", err)
-			return
-		}
-	}
+	//---------------------- example task blueprint
+	//	exampleTask, exampleTaskErr := jobs.TaskExample("your data ")
+	//	if exampleTaskErr == nil {
+	//		_, err := schedule.Register("@every 3s", exampleTask)
+	//		if err != nil {
+	//			log.Println("[x] error `exampleTaskErr` run schedule:", err)
+	//			return
+	//		}
+	//}
 	//------------------------
+
+	_, err := schedule.Register("@every 1m", adminJob.TaskCancelPendingOrders())
+	if err != nil {
+		log.Println("CancelPendingOrders err:", err)
+	}
+
 }
