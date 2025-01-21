8a0f7afeab61b7385fc20861694c9b3a34f72718
abbas <abbasmomeny1994@gmail.com>
2025-01-21 20:23:59 +0330

    ref: event_manager.go

--- internal/events/event_manager.go
+++ internal/events/event_manager.go
@@ -3,7 +3,16 @@ package events
 import (
 	"context"
 	"fmt"
+	"github.com/hibiken/asynq"
+	"github.com/redis/go-redis/v9"
+	"go.mongodb.org/mongo-driver/mongo"
+	"gorm.io/gorm"
 	"sync"
+)
+
+var (
+	once                 sync.Once
+	eventManagerInstance *EventManager
 )
 
 type ListenerFunc func(ctx context.Context, data any)
@@ -11,18 +20,22 @@ type ListenerFunc func(ctx context.Context, data any)
 type eventName string
 
 type EventManager struct {
+	dep       *EventManagerDep
 	listeners map[eventName][]ListenerFunc
 	mu        sync.RWMutex
 }
 
-var (
-	once                 sync.Once
-	eventManagerInstance *EventManager
-)
+type EventManagerDep struct {
+	AsynqClient *asynq.Client
+	DB          *gorm.DB
+	RedisClient *redis.Client
+	MongoClient *mongo.Client
+}
 
-func NewEventManager() *EventManager {
+func NewEventManager(dep *EventManagerDep) *EventManager {
 	once.Do(func() {
 		eventManagerInstance = &EventManager{
+			dep:       dep,
 			listeners: make(map[eventName][]ListenerFunc),
 		}
 	})
@@ -48,4 +61,8 @@ func (em *EventManager) Emit(ctx context.Context, eventName eventName, data any,
 	} else {
 		fmt.Printf("No listeners found for event: %s\n", eventName)
 	}
+}
+
+func GetDep() *EventManagerDep {
+	return eventManagerInstance.dep
 }
