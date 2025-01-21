f64400c543fd71e5759e744c43abf44375c9c510
abbas <abbasmomeny1994@gmail.com>
2025-01-21 20:45:51 +0330

    add: main for events

--- internal/events/main.go
+++ internal/events/main.go
@@ -1,17 +1,31 @@
 package events
 
-import "context"
+import (
+	"context"
+	"log"
+	"shop/internal/pkg/bootstrap"
+)
 
 // e.g., this main.go file is just an example of how to use events and listeners.
 
 func main() {
+	dep, err := bootstrap.Initialize()
+	if err != nil {
+		log.Fatalln("fail to initialize bootstrap in event main.go file :", err)
+	}
 
-	em := NewEventManager()
+	evntDep := EventManagerDep{
+		AsynqClient: dep.AsynqClient,
+		DB:          dep.DB,
+		RedisClient: dep.RedisClient,
+		MongoClient: dep.MongoClient,
+	}
+	em := NewEventManager(&evntDep)
 
 	//register events
 	RegisterEvents(em)
 
-	//in your routes e.g. /users/welcome
+	// in your routes e.g. /users/welcome
 	userWelcomePayload := map[string]any{
 		"user_id": 1,
 		"name":    "ali",
