672a3871f530590544f2c02e7c69a810af159b8c
abbas <abbasmomeny1994@gmail.com>
2025-01-26 01:19:05 +0330

    add comments

--- cmd/job/worker/main.go
+++ cmd/job/worker/main.go
@@ -10,11 +10,13 @@ import (
 )
 
 func main() {
+	//load bootstrap
 	dep, err := bootstrap.Initialize()
 	if err != nil {
 		log.Fatalf("[x] failed to initialize dependencies: %v", err)
 	}
 
+	//config asynq server
 	server := asynq.
 		NewServer(
 			asynq.RedisClientOpt{Addr: fmt.Sprintf("%s:%s", os.Getenv("REDIS_HOSTNAME"), os.Getenv("REDIS_PORT"))},
@@ -36,7 +38,7 @@ func main() {
 	//mux.HandleFunc(publicJob.TypeExample, publicJob.HandleExampleTask)
 
 	mux.Handle(adminJob.CancelPendingOrders, adminJob.NewCancelJob(dep))
-
+	//mux.Handle(jobs.TypeSendEmail, jobs.NewSendEmailJob(dep))
 	//>>>>> run serve r<<<<<
 	err = server.Run(mux)
 	if err != nil {
