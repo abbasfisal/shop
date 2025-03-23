e4ce822458c0a9622dd5ef36dea7ff1cb544ceff
abbas <abbasmomeny1994@gmail.com>
2025-03-23 16:13:18 +0330

    add: js to work search in mobile screen size

---
+++ assets/shop/js/tseaerch/tsearch_small.js
@@ -0,0 +1,29 @@
+document.getElementById("searchQuery_small").addEventListener("input", function () {
+    const query = this.value;
+
+    if (query.length > 0) {
+        fetch(`/tsearch?q=${query}`)
+            .then(response => response.json())
+            .then(data => {
+                const resultsList = document.getElementById("results_small");
+                resultsList.innerHTML = '';
+
+                if (data.results && Array.isArray(data.results)) {
+                    data.results.forEach(result => {
+                        const highlightedTitle = result.title.replace(new RegExp(query, 'gi'), (match) => {
+                            return `<span class="highlight_search">${match}</span>`;
+                        });
+
+                        const li = document.createElement("li");
+                        li.innerHTML = `<a  href="${result.link}">${highlightedTitle}</a>`;
+                        resultsList.appendChild(li);
+                    });
+                } else {
+                    resultsList.innerHTML = `<li>نتیجه‌ای یافت نشد.</li>`;
+                }
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    }
+});
