80a9f147c643799a0054612cff789ae37ddff2f2
abbas <abbasmomeny1994@gmail.com>
2025-04-10 13:04:36 +0330

    ref: LoadMenu middleware

--- internal/modules/public/middlewares/load_menu.go
+++ internal/modules/public/middlewares/load_menu.go
@@ -1,23 +1,25 @@
 package middlewares
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
 	"net/http"
 	"shop/internal/modules/public/services/home"
-	"shop/internal/pkg/custom_error"
+	"shop/internal/pkg/logging"
 )
 
 func LoadMenu(homeSrv home.HomeServiceInterface) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		menu, err := homeSrv.GetMenu(c)
 		if err != nil {
-			fmt.Println("~~~~~~~~ loadMenu Middleware err:", err, "\n~~~~~~~~~~~~~~~~~")
-			c.JSON(http.StatusInternalServerError, gin.H{
-				"message": custom_error.SomethingWrongHappened,
-			})
+
+			logging.Log.
+				WithFields(logrus.Fields{"function": "LoadMenu"}).
+				WithError(err).Fatal("load menu failed")
+
+			c.HTML(http.StatusInternalServerError, "500", gin.H{})
 			c.Abort()
-			return
+
 		}
 		c.Set("menu", menu)
 		c.Next()
